a0c65ba282cfaba5c6eaa080327a1242e60a6677
Matt Williams <matt.seongjun@gmail.com>
2019-06-11 15:24:43 -0400

    Check comments in recipes_controller and _form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,30 @@
 class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
 
-  def index
-    @recipes = Recipe.all
-  end
+    def index
+        @recipes = Recipe.all
+    end
 
-  def new
-    @recipe = Recipe.new
-  end
+    def new
+        @recipe = Recipe.new
+        @recipe.ingredients.build(name: "first")
+        @recipe.ingredients.build(name: "second")
+        # names don't matter
+    end
 
-  def create
-  end
+    def create
+        @recipe = Recipe.create(
+            recipe_params(
+                :title, ingredients_attributes: [:name, :quantity]
+                # must be <model>s_attributes
+            )
+        )
+    end
+
+    private
+        def recipe_params(*args)
+            params.require(:recipe).permit(args)
+        end
 end
